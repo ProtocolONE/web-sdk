f7ef40d87c14012ce1a946516bd29e9fa23ef09b
nikolay.bondarenko <misterionkell@gmail.com>
2018-09-04 16:43:09 +0300

    Add FNV-1a Hash implementation for finger print checksum

---
+++ src/functions/fnv64a.js
@@ -0,0 +1,87 @@
+/**
+ * FNV-1a Hash implementation (32, 64, 128, 256, 512, and 1024 bit)
+ * @author Travis Webb <me@traviswebb.com>
+ * @see http://tools.ietf.org/html/draft-eastlake-fnv-06
+ */
+let hl = [];
+
+for(let i=0; i < 256; i++){
+    hl[i] = ((i >> 4) & 15).toString(16) + (i & 15).toString(16);
+}
+
+export default function fnv64_1a_fast(str) {
+    let i, l = str.length - 3, t0 = 0, v0 = 0x2325, t1 = 0, v1 = 0x8422, t2 = 0, v2 = 0x9ce4, t3 = 0, v3 = 0xcbf2;
+
+    for (i = 0; i < l;) {
+        v0 ^= str.charCodeAt(i++);
+        t0 = v0 * 435;
+        t1 = v1 * 435;
+        t2 = v2 * 435;
+        t3 = v3 * 435;
+        t2 += v0 << 8;
+        t3 += v1 << 8;
+        t1 += t0 >>> 16;
+        v0 = t0 & 65535;
+        t2 += t1 >>> 16;
+        v1 = t1 & 65535;
+        v3 = (t3 + (t2 >>> 16)) & 65535;
+        v2 = t2 & 65535;
+        v0 ^= str.charCodeAt(i++);
+        t0 = v0 * 435;
+        t1 = v1 * 435;
+        t2 = v2 * 435;
+        t3 = v3 * 435;
+        t2 += v0 << 8;
+        t3 += v1 << 8;
+        t1 += t0 >>> 16;
+        v0 = t0 & 65535;
+        t2 += t1 >>> 16;
+        v1 = t1 & 65535;
+        v3 = (t3 + (t2 >>> 16)) & 65535;
+        v2 = t2 & 65535;
+        v0 ^= str.charCodeAt(i++);
+        t0 = v0 * 435;
+        t1 = v1 * 435;
+        t2 = v2 * 435;
+        t3 = v3 * 435;
+        t2 += v0 << 8;
+        t3 += v1 << 8;
+        t1 += t0 >>> 16;
+        v0 = t0 & 65535;
+        t2 += t1 >>> 16;
+        v1 = t1 & 65535;
+        v3 = (t3 + (t2 >>> 16)) & 65535;
+        v2 = t2 & 65535;
+        v0 ^= str.charCodeAt(i++);
+        t0 = v0 * 435;
+        t1 = v1 * 435;
+        t2 = v2 * 435;
+        t3 = v3 * 435;
+        t2 += v0 << 8;
+        t3 += v1 << 8;
+        t1 += t0 >>> 16;
+        v0 = t0 & 65535;
+        t2 += t1 >>> 16;
+        v1 = t1 & 65535;
+        v3 = (t3 + (t2 >>> 16)) & 65535;
+        v2 = t2 & 65535;
+    }
+
+    while (i < l + 3) {
+        v0 ^= str.charCodeAt(i++);
+        t0 = v0 * 435;
+        t1 = v1 * 435;
+        t2 = v2 * 435;
+        t3 = v3 * 435;
+        t2 += v0 << 8;
+        t3 += v1 << 8;
+        t1 += t0 >>> 16;
+        v0 = t0 & 65535;
+        t2 += t1 >>> 16;
+        v1 = t1 & 65535;
+        v3 = (t3 + (t2 >>> 16)) & 65535;
+        v2 = t2 & 65535;
+    }
+
+    return hl[v3 >> 8] + hl[v3 & 255] + hl[v2 >> 8] + hl[v2 & 255] + hl[v1 >> 8] + hl[v1 & 255] + hl[v0 >> 8] + hl[v0 & 255];
+}
